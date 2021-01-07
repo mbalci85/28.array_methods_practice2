d1a88e9d7c6338aa590a10d85eb8c2410ddc38c2
Mustafa Balci <72220498+mbalci85@users.noreply.github.com>
2021-01-07 23:32:32 +0000

    added files and func

---
+++ script.js
@@ -0,0 +1,13 @@
+let streets = [
+	'Abchurch Lane',
+	"Adam's Court",
+	'Addle Hill',
+	'Addle Lane',
+	'Alban Highwalk',
+];
+
+const getLanes = (arr) => {
+	return arr.filter((lane) => lane.includes('Lane'));
+};
+
+console.log(ge);
