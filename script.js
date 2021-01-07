5f033e62af8f1c03256b1e770ed4d87abfde5169
Mustafa Balci <72220498+mbalci85@users.noreply.github.com>
2021-01-07 23:42:52 +0000

    added second method

--- script.js
+++ script.js
@@ -10,4 +10,19 @@ const getLanes = (arr) => {
 	return arr.filter((lane) => lane.includes('Lane'));
 };
 
-console.log(ge);
+console.log(getLanes(streets));
+
+let data1 = [false, 16, 27, 'array', 35, null, 29];
+let data2 = [true, 43, 'age', '...', 34, 'UK'];
+
+const sortAges = (arr) => {
+	let res = [];
+	arr.forEach((data) => {
+		if (typeof data == 'number') {
+			res.push(data);
+		}
+	});
+	return res;
+};
+
+console.log(sortAges(data1));
